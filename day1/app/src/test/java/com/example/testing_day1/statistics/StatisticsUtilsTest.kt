37362dbbd309a1c7d51b6b0b6dc2cf20e48c1eec
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-19 17:00:13 +0300

    Finished First Day

--- day1/app/src/test/java/com/example/testing_day1/statistics/StatisticsUtilsTest.kt
+++ day1/app/src/test/java/com/example/testing_day1/statistics/StatisticsUtilsTest.kt
@@ -1,14 +1,14 @@
 package com.example.testing_day1.statistics
 
 import com.example.testing_day1.data.Task
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
 
 
 class StatisticsUtilsTest {
 
     @Test
-    fun getActiveAndCompleteStats_noComplete_hundredActiveZeroComplete()
+    fun getActiveAndCompleteStats_zeroComplete_returnAllActiveZeroComplete()
     {
         //Given
         val tasks = listOf<Task>(Task(isCompleted = false))
@@ -18,6 +18,41 @@ class StatisticsUtilsTest {
         assertEquals(0f,result.completedTasksPercent)
         assertEquals(100f,result.activeTasksPercent)
 
+    }
+
+    @Test
+    fun getActiveAndCompleteStats_twoComplete_returnSixtyActiveFortyComplete()
+    {
+        //Given
+        val tasks = listOf<Task>(Task(isCompleted = true),Task(isCompleted = true),Task(isCompleted = false),Task(isCompleted = false),Task(isCompleted = false))
+        //When
+        val result = getActiveAndCompletedStats(tasks)
+        //Then
+        assertEquals(40f,result.completedTasksPercent)
+        assertEquals(60f,result.activeTasksPercent)
+
+    }
+    @Test
+    fun getActiveAndCompleteStats_emptyList_returnZeroActiveZeroComplete()
+    {
+        //Given
+        val tasks = emptyList<Task>()
+        //When
+        val result = getActiveAndCompletedStats(tasks)
+        //Then
+        assertEquals(0f,result.completedTasksPercent)
+        assertEquals(0f,result.activeTasksPercent)
+    }
+    @Test
+    fun getActiveAndCompleteStats_nullList__returnZeroActiveZeroComplete()
+    {
+        //Given
+        val tasks = null
+        //When
+        val result = getActiveAndCompletedStats(tasks)
+        //Then
+        assertEquals(0f,result.completedTasksPercent)
+        assertEquals(0f,result.activeTasksPercent)
     }
 
 }
---
+++ day1/app/src/test/java/com/example/testing_day1/tasks/TasksViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.example.testing_day1.tasks
+
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+
+    lateinit var tasksViewModel : TasksViewModel
+
+    @Before
+    fun setup(){
+        val context = ApplicationProvider.getApplicationContext() as Application
+        tasksViewModel = TasksViewModel(context)
+    }
+
+    @Test
+    fun addNewTask_newTaskEventIsNotNull()
+    {
+        //Given
+        //When
+        tasksViewModel.addNewTask()
+        //Then
+        val result = tasksViewModel.newTaskEvent.getOrAwaitValue{}
+
+        assertThat(result,(not(nullValue())))
+    }
+
+    @Test
+    fun setFilter_AllTask()
+    {
+        //Given
+        //When
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+        //Then
+        val result = tasksViewModel.currentFilteringLabel.getOrAwaitValue{}
+
+        assertThat(result,(not(nullValue())))
+    }
+
+}
